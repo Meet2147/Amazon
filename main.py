4e156da4ff00b7f45d1ff5763a1c8e5c66064da5
Meet2147 <meetjethwa3@gmail.com>
2022-09-28 12:24:08 +0530

    added tags

--- main.py
+++ main.py
@@ -17,24 +17,26 @@ def root():
 @app.get("/product_details")
 def profile(link: str):
 # url = input("Please enter the URL of the product: ")
-    header = {
+    headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
 }
 
-    response = requests.get(link, headers=header)
+    response = requests.get(link, headers=headers)
 
-    soup = BeautifulSoup(response.content, "lxml")
+    soup = BeautifulSoup(response.content, "html.parser")
+    soup2 = BeautifulSoup(soup.prettify(), "html.parser")
+    
 
-    title = soup.find("span",attrs={"id": 'productTitle'})
-    title_value = title.string
- 
-    title_string = title_value.strip().replace(',', '')
-    print("product Title = ", title_string)
+    title = soup2.find(id='productTitle').get_text()
+    title = title.strip()
+    print("product Title = ", title)
     
-    price = soup.find("span", attrs={'class': 'offscreen'}).string.strip().replace(',', '')
+    price = soup2.find('span',{'class':'a-offscreen'}).text
+    price = price.strip()[1:]
+    
 
-    discount = soup.find("span", attrs={'class': 'a-size-large a-color-price savingPriceOverride aok-align-center reinventPriceSavingsPercentageMargin savingsPercentage'})
+    discount = soup.find("span", {'class': 'a-size-large a-color-price savingPriceOverride aok-align-center reinventPriceSavingsPercentageMargin savingsPercentage'}).text
+    discount = discount.strip()
     
-    
-    return {"Title":title_string,"Price":price,"Discount":discount}
+    return {"Title":title,"Price":price, "Discount":discount}
